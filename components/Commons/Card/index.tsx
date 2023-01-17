7772573a43f47a8da0635bad06b88b6368b6e91c
Yusril Hasan <79882296+wahyubudii@users.noreply.github.com>
2023-01-17 15:30:13 +0700

    fix error

--- components/Commons/Card/index.tsx
+++ components/Commons/Card/index.tsx
@@ -5,22 +5,23 @@ import Link from "next/link";
 export default function Card({ data, index }: any): JSX.Element {
   return (
     <Link
-      href="#"
+      href={`/blog/${data.slug}`}
       key={index}
       className="border border-slate-600 rounded overflow-hidden text-white hover:scale-105 transition"
     >
       <div className="flex items-center justify-between">
         <div className="mx-10">
-          <h2 className="text-xl font-bold">{data.title}</h2>
-          <p className="pt-2 text-xs">{data.description}</p>
-          <p className="text-xs mt-4">{data.date}</p>
+          <h2 className="text-xl font-bold">{data.frontMatter.title}</h2>
+          <p className="pt-2 text-[12px]">{data.frontMatter.description}</p>
+          <p className="text-xs mt-4">{data.frontMatter.date}</p>
         </div>
         <Image
-          src={data.image}
+          src={data.frontMatter.thumbnailUrl}
           alt="blog logo"
           width={250}
           height={100}
           priority
+          className="w-auto h-auto"
         />
       </div>
     </Link>
